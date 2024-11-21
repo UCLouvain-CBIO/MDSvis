fcbba1bd8d72a64fbd50e383e5c65da0d525af75
Andrea  Vicini <andreavicini@MacBook-Pro-de-Andrea.local>
2024-11-21 16:23:46 +0100

    two separate panels for visualization and general settings + new controls.

--- app.R
+++ app.R
@@ -8,15 +8,46 @@ stats <- readRDS(file = "/Users/andreavicini/Desktop/InputObjects/HBV_stats.rds"
 
 ui <- fluidPage(
   titlePanel("Plot of Metric MDS object"),
-  sidebarLayout(
-    sidebarPanel(
-      selectInput("axis1", "X-axis coordinate:", choices = seq_len(nDim(mdsObj)), selected = 1),
-      selectInput("axis2", "Y-axis coordinate:", choices = seq_len(nDim(mdsObj)), selected = 2),
-      selectInput("colourBy", "Colour by:", choices = colnames(pData), selected = colnames(pData)[1]),
-      selectInput("labelBy", "Label by:", choices = colnames(pData), selected = colnames(pData)[2])
+  tabsetPanel(
+    id = "tabs", # Tab ID
+    # View Tab
+    tabPanel("View",
+             sidebarLayout(
+               sidebarPanel(
+                 selectInput("axis1", "X-axis coordinate:", choices = seq_len(nDim(mdsObj)), selected = 1),
+                 selectInput("axis2", "Y-axis coordinate:", choices = seq_len(nDim(mdsObj)), selected = 2),
+                 selectInput("colourBy", "Colour by:", choices = colnames(pData), selected = colnames(pData)[1]),
+                 selectInput("labelBy", "Label by:", choices = colnames(pData), selected = colnames(pData)[2]),
+                 selectInput("shapeBy", "Shape by:", choices = colnames(pData), selected = colnames(pData)[2]),
+                 checkboxInput("biplot", "biplot", value = FALSE),
+                 numericInput("arrowThreshold", "arrowThreshold", value = 0.8 , step = 0.1),
+                 checkboxInput("flipXAxis", "Flip X-axis", value = FALSE),
+                 checkboxInput("flipYAxis", "Flip Y-axis", value = FALSE)
+               ),
+               mainPanel(
+                 plotOutput("mdsPlot", height = "500px")
+               )
+             )
     ),
-    mainPanel(
-      plotOutput("mdsPlot")
+    # General Settings Tab
+    tabPanel("General Settings",
+             sidebarLayout(
+               sidebarPanel(
+                 numericInput("pointLabelSize", "Point Label Size",
+                              value = 3.88, step = 0.1),
+                 checkboxInput("displayPointLabels", "displayPointLabels", value = TRUE),
+                 checkboxInput("repelPointLabels", "repelPointLabels", value = TRUE),
+                 checkboxInput("displayPseudoRSq", "Display pseudo RSquare", value = TRUE),
+                 numericInput("pointSize", "Point size", value = 1 , step = 0.1),
+                 checkboxInput("pointSizeReflectingStress", "pointSizeReflectingStress", value = FALSE),
+                 checkboxInput("displayArrowLabels", "displayArrowLabels", value = TRUE),
+                 numericInput("arrowLabelSize", "arrowLabelSize", value = 3.88, step = 0.1),
+                 checkboxInput("repelArrowLabels", "repelArrowLabels", value = FALSE)
+               ),
+               mainPanel(
+                 p("")
+               )
+             )
     )
   )
 )
@@ -28,8 +59,23 @@ server <- function(input, output, session) {
       mdsObj = mdsObj,
       pData = pData,
       projectionAxes = projectionAxes,
+      biplot = input$biplot,
+      extVariables = stats$median, # how to choose variable?
       pDataForColour = input$colourBy,
-      pDataForLabel = input$labelBy
+      pDataForLabel = input$labelBy,
+      pDataForShape = input$shapeBy,
+      flipXAxis = input$flipXAxis,
+      flipYAxis = input$flipYAxis,
+      pointLabelSize = input$pointLabelSize,
+      displayPointLabels = input$displayPointLabels,
+      repelPointLabels = input$repelPointLabels,
+      displayPseudoRSq = input$displayPseudoRSq,
+      pointSizeReflectingStress = input$pointSizeReflectingStress,
+      pointSize = input$pointSize,
+      displayArrowLabels = input$displayArrowLabels,
+      arrowLabelSize = input$arrowLabelSize,
+      repelArrowLabels = input$repelArrowLabels,
+      arrowThreshold = input$arrowThreshold
     )
   })
 }
