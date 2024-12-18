a3acc081c69a235e455c41de3f43caafad791def
Andrea  Vicini <andrea.vicini@uclouvain.be>
2024-12-18 10:32:06 +0100

    remove unnecessary lines + long lines indentation

--- inst/app/app.R
+++ inst/app/app.R
@@ -1,7 +1,3 @@
-# mdsObj <- readRDS(system.file("app/data/HBV_mdsObj.rds", package = "MDSgui"))
-# pData <- readRDS(system.file("app/data/HBV_phenoData.rds", package = "MDSgui"))
-# stats <- readRDS(system.file("app/data/HBV_stats.rds", package = "MDSgui"))
-
 ui <- fluidPage(
   shinyjs::useShinyjs(),
   titlePanel("Plot of Metric MDS object"),
@@ -10,9 +6,12 @@ ui <- fluidPage(
     tabPanel("Input files",
              sidebarLayout(
                sidebarPanel(
-                 fileInput("mdsObjFile", "Choose MDS object file", accept = ".rds"),
-                 fileInput("pDataFile", "Choose phenodataData file", accept = ".rds"),
-                 fileInput("statsFile", "Choose stats file", accept = ".rds")
+                 fileInput("mdsObjFile", "Choose MDS object file",
+                           accept = ".rds"),
+                 fileInput("pDataFile", "Choose phenodataData file",
+                           accept = ".rds"),
+                 fileInput("statsFile", "Choose stats file",
+                           accept = ".rds")
                ),
                mainPanel(
                  p("")
@@ -81,34 +80,6 @@ ui <- fluidPage(
   )
 )
 
-# server <- function(input, output, session) {
-#   output$mdsPlot <- renderPlot({
-#     CytoMDS::ggplotSampleMDS(
-#       mdsObj = mdsObj,
-#       pData = pData,
-#       projectionAxes = c(as.integer(input$axis1), as.integer(input$axis2)),
-#       biplot = input$biplot,
-#       extVariables = stats$median, # how to choose variable?
-#       pDataForColour = input$colourBy,
-#       pDataForLabel = input$labelBy,
-#       pDataForShape = input$shapeBy,
-#       flipXAxis = input$flipXAxis,
-#       flipYAxis = input$flipYAxis,
-#       pointLabelSize = input$pointLabelSize,
-#       displayPointLabels = input$displayPointLabels,
-#       repelPointLabels = input$repelPointLabels,
-#       displayPseudoRSq = input$displayPseudoRSq,
-#       pointSizeReflectingStress = input$pointSizeReflectingStress,
-#       pointSize = input$pointSize,
-#       displayArrowLabels = input$displayArrowLabels,
-#       arrowLabelSize = input$arrowLabelSize,
-#       repelArrowLabels = input$repelArrowLabels,
-#       arrowThreshold = input$arrowThreshold,
-#       pDataForAdditionalLabelling = c("group", "patientId")
-#     )
-#   })
-# }
-
 server <- function(input, output, session) {
   mdsObj <- reactiveVal(NULL)
   pData <- reactiveVal(NULL)
@@ -143,55 +114,34 @@ server <- function(input, output, session) {
         shinyjs::enable("labelBy")
         shinyjs::enable("shapeBy")
       }
-      # if (input$biplot == TRUE) {
-      #   shinyjs::enable("plotly")
-      # }
     }
   })
-
-  # conditionalPanel(
-  #   condition = "input.biplot == false",
-  #   checkboxInput("plotlytooltipping", "Plotly", value = FALSE)
-  # ),
-  # conditionalPanel(
-  #   condition = "input.plotlytooltipping == true",
-  #   selectInput("pDataForAdditionalLabelling",
-  #               "pDataForAdditionalLabelling:",
-  #               choices = NULL, multiple = TRUE)
-  # ),
-  # conditionalPanel(
-  #   condition = "input.biplot == true",
-  #   selectInput("extVariables", "Select stat:", choices = NULL),
-  #   numericInput("arrowThreshold", "arrowThreshold", value = 0.8,
-  #                step = 0.1)
-  # ),
-  # observeEvent(input$plotlytooltipping, {
-  #   if (input$plotlytooltipping == TRUE) {
-  #     shinyjs::disable("biplot")
-  #   } else {
-  #     shinyjs::enable("biplot")
-  #   }
-  # })
 
   observeEvent(input$mdsObjFile, {
     req(input$mdsObjFile)
     mdsObj(readRDS(input$mdsObjFile$datapath))
-    updateSelectInput(session, "axis1", choices = seq_len(CytoMDS::nDim(mdsObj())), selected = 1)
-    updateSelectInput(session, "axis2", choices = seq_len(CytoMDS::nDim(mdsObj())), selected = 2)
+    updateSelectInput(session, "axis1",
+                      choices = seq_len(CytoMDS::nDim(mdsObj())), selected = 1)
+    updateSelectInput(session, "axis2",
+                      choices = seq_len(CytoMDS::nDim(mdsObj())), selected = 2)
   })
 
   observeEvent(input$pDataFile, {
     req(input$pDataFile)
     pData(readRDS(input$pDataFile$datapath))
-    updateSelectInput(session, "colourBy", choices = colnames(pData()), selected = colnames(pData())[1])
-    updateSelectInput(session, "labelBy", choices = colnames(pData()), selected = colnames(pData())[2])
-    updateSelectInput(session, "shapeBy", choices = colnames(pData()), selected = colnames(pData())[2])
+    updateSelectInput(session, "colourBy", choices = colnames(pData()),
+                      selected = colnames(pData())[1])
+    updateSelectInput(session, "labelBy", choices = colnames(pData()),
+                      selected = colnames(pData())[2])
+    updateSelectInput(session, "shapeBy", choices = colnames(pData()),
+                      selected = colnames(pData())[2])
   })
 
   observeEvent(input$statsFile, {
     req(input$statsFile)
     stats(readRDS(input$statsFile$datapath))
-    updateSelectInput(session, "extVariables", choices = names(stats()), selected = names(stats())[1])
+    updateSelectInput(session, "extVariables", choices = names(stats()),
+                      selected = names(stats())[1])
   })
 
   p <- reactive({
@@ -234,7 +184,7 @@ server <- function(input, output, session) {
         repelArrowLabels = input$repelArrowLabels,
         arrowThreshold = input$arrowThreshold
       )
-    } else if (!is.null(input$pDataFile) && is.null(input$statsFile)){ # no biplot
+    } else if (!is.null(input$pDataFile) && is.null(input$statsFile)){ #no biplot
       CytoMDS::ggplotSampleMDS(
         mdsObj = mdsObj(),
         pData = pData(),
